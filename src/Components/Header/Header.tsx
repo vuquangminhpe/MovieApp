20b8905b39d2b15d1abdb9a26150e4eaabc001eb
minhminh <minhvqhe176726@fpt.edu.vn>
2024-09-21 14:32:10 +0700

    add Footer

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -39,164 +39,164 @@ export default function Header() {
   // })
 
   return (
-    <div className='relative bg-blue-950 h-[100px] w-full p-4'>
-      <div className='flex md:justify-between items-center p-4'>
-        <div className='flex'>
-          <Link to={path.home} className='mr-4 min-w-[154px]'>
-            <img
-              src='https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg'
-              alt='The Movie Database (TMDB)'
-              width='154'
-              height='20'
-            />
-          </Link>
-          <Popover
-            children='Movies'
-            className='text-white w-[60px] mr-3 font-bold ml-2'
-            renderPopover={
-              <div className='shadow-sm max-w-52 w-40  text-sm font-sans gap-1 border rounded-xl border-gray-300 bg-white text-black flex flex-col justify-center text-left items-center'>
-                <Link to='' className='hover:bg-gray-200 p-2 rounded-xl  w-full'>
-                  Popdivar
-                </Link>
-                <Link to='' className='hover:bg-gray-200 p-2  w-full'>
-                  Now Playing
-                </Link>
-                <Link to='' className='hover:bg-gray-200 p-2 w-full'>
-                  Upcoming
-                </Link>
-                <Link to='' className='hover:bg-gray-200 p-2 rounded-xl w-full'>
-                  Top Rated
-                </Link>
-              </div>
-            }
+    <div className='relative bg-blue-950 flex h-[100px] md:justify-between items-center w-full p-4'>
+      <div className='flex'>
+        <Link to={path.home} className='mr-4 min-w-[154px]'>
+          <img
+            src='https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg'
+            alt='The Movie Database (TMDB)'
+            width='154'
+            height='20'
           />
-          <Popover
-            children='TV Shows'
-            className='text-white w-[80px] mr-3 font-bold ml-2'
-            renderPopover={
-              <div className=' shadow-sm max-w-52 w-40  text-sm font-sans gap-1 border rounded-xl border-gray-300 bg-white text-black flex flex-col justify-center text-left items-center'>
-                <Link to='' className='hover:bg-gray-200 p-2 rounded-xl  w-full'>
-                  Popular
-                </Link>
-                <Link to='' className='hover:bg-gray-200 p-2  w-full'>
-                  On TV
-                </Link>
-                <Link to='' className='hover:bg-gray-200 p-2 w-full'>
-                  Airing Today
-                </Link>
-                <Link to='' className='hover:bg-gray-200 p-2 rounded-xl w-full'>
-                  Top Rated
-                </Link>
-              </div>
-            }
-          />
-          <Popover
-            children='People'
-            className='text-white w-[60px] mr-3 font-bold ml-2'
-            renderPopover={
-              <div className=' shadow-sm max-w-52 w-40  text-sm font-sans gap-1 border rounded-xl border-gray-300 bg-white text-black flex flex-col justify-center text-left items-center'>
-                <Link to='' className='hover:bg-gray-200 p-2 rounded-xl  w-full'>
-                  Popular People
-                </Link>
-              </div>
-            }
-          />
-          <Popover
-            children='More'
-            className='text-white w-[60px] mr-3 font-bold ml-2'
-            renderPopover={
-              <div className=' shadow-sm max-w-52 w-40  text-sm font-sans gap-1 border rounded-xl border-gray-300 bg-white text-black flex flex-col justify-center text-left items-center'>
-                <Link to='' className='hover:bg-gray-200 p-2 rounded-xl  w-full'>
-                  Discussions
-                </Link>
-                <Link to='' className='hover:bg-gray-200 p-2  w-full'>
-                  Leaderbroard
-                </Link>
-                <Link to='' className='hover:bg-gray-200 p-2 w-full'>
-                  Support
-                </Link>
-                <Link to='' className='hover:bg-gray-200 p-2 rounded-xl w-full'>
-                  API
-                </Link>
-              </div>
-            }
-          />
-        </div>
-        <div className='flex ml-3 mt-1'>
-          <Popover
-            children={
-              <div className='text-white '>
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  fill='none'
-                  viewBox='0 0 24 24'
-                  strokeWidth={1.5}
-                  stroke='currentColor'
-                  className='size-6'
-                >
-                  <path strokeLinecap='round' strokeLinejoin='round' d='M12 4.5v15m7.5-7.5h-15' />
-                </svg>
-              </div>
-            }
-            show={true}
-            onEvent='onClick'
-            renderPopover={
-              <div className=' shadow-sm max-w-52 w-40  text-sm font-sans gap-1 border rounded-xl border-gray-300 bg-white text-black flex flex-col justify-center text-left items-center'>
-                {' '}
-                <Link to='' className='hover:bg-blue-950 p-2 w-full rounded-xl hover:text-white'>
-                  Add New Movie
-                </Link>
-                <Link to='' className='hover:hover:bg-blue-950 hover:text-white p-2 rounded-xl w-full'>
-                  Add New TV Show
-                </Link>
-              </div>
-            }
-          />
-
-          <Popover
-            children='VI'
-            className='border border-white w-7 text-center text-white font-bold mx-6 hover:text-black hover:bg-white cursor-pointer'
-            renderPopover={
-              <div className='w-64 h-26 p-3 bg-white border border-gray-200 shadow-sm rounded-sm'>
-                <h2 className='font-bold'>Language Preferences</h2>
-                <div>
-                  <select className='mt-2 bg-gray-200/90 rounded-sm shadow-sm w-full p-2'>
-                    <option value='Tiếng việt'>Tiếng việt</option>
-                    <option value='English'>English</option>
-                  </select>
-                </div>
-              </div>
-            }
-            onEvent='onClick'
-            show={true}
-          />
-          <Popover
-            onEvent='onClick'
-            show={true}
-            children={
-              <img
+        </Link>
+        <Popover
+          children='Movies'
+          className='text-white w-[60px] mr-3 font-bold ml-2'
+          renderPopover={
+            <div className='shadow-sm max-w-52 w-40  text-sm font-sans gap-1 border rounded-xl border-gray-300 bg-white text-black flex flex-col justify-center text-left items-center'>
+              <Link to='' className='hover:bg-gray-200 p-2 rounded-xl  w-full'>
+                Popdivar
+              </Link>
+              <Link to='' className='hover:bg-gray-200 p-2  w-full'>
+                Now Playing
+              </Link>
+              <Link to='' className='hover:bg-gray-200 p-2 w-full'>
+                Upcoming
+              </Link>
+              <Link to='' className='hover:bg-gray-200 p-2 rounded-xl w-full'>
+                Top Rated
+              </Link>
+            </div>
+          }
+        />
+        <Popover
+          children='TV Shows'
+          className='text-white w-[80px] mr-3 font-bold ml-2'
+          renderPopover={
+            <div className=' shadow-sm max-w-52 w-40  text-sm font-sans gap-1 border rounded-xl border-gray-300 bg-white text-black flex flex-col justify-center text-left items-center'>
+              <Link to='' className='hover:bg-gray-200 p-2 rounded-xl  w-full'>
+                Popular
+              </Link>
+              <Link to='' className='hover:bg-gray-200 p-2  w-full'>
+                On TV
+              </Link>
+              <Link to='' className='hover:bg-gray-200 p-2 w-full'>
+                Airing Today
+              </Link>
+              <Link to='' className='hover:bg-gray-200 p-2 rounded-xl w-full'>
+                Top Rated
+              </Link>
+            </div>
+          }
+        />
+        <Popover
+          children='People'
+          className='text-white w-[60px] mr-3 font-bold ml-2'
+          renderPopover={
+            <div className=' shadow-sm max-w-52 w-40  text-sm font-sans gap-1 border rounded-xl border-gray-300 bg-white text-black flex flex-col justify-center text-left items-center'>
+              <Link to='' className='hover:bg-gray-200 p-2 rounded-xl  w-full'>
+                Popular People
+              </Link>
+            </div>
+          }
+        />
+        <Popover
+          children='More'
+          className='text-white w-[60px] mr-3 font-bold ml-2'
+          renderPopover={
+            <div className=' shadow-sm max-w-52 w-40  text-sm font-sans gap-1 border rounded-xl border-gray-300 bg-white text-black flex flex-col justify-center text-left items-center'>
+              <Link to='' className='hover:bg-gray-200 p-2 rounded-xl  w-full'>
+                Discussions
+              </Link>
+              <Link to='' className='hover:bg-gray-200 p-2  w-full'>
+                Leaderbroard
+              </Link>
+              <Link to='' className='hover:bg-gray-200 p-2 w-full'>
+                Support
+              </Link>
+              <Link to='' className='hover:bg-gray-200 p-2 rounded-xl w-full'>
+                API
+              </Link>
+            </div>
+          }
+        />
+      </div>
+      <div className='flex ml-3 mt-1'>
+        <Popover
+          children={
+            <div className='text-white '>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                strokeWidth={1.5}
+                stroke='currentColor'
                 className='size-6'
-                src='https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-441-bell-9cd2af257b98c4af3460078777d8e38a5e12bca89704eeac2f39273afcbd06ff.svg'
-                alt=''
-              />
-            }
-            renderPopover={
-              <div className=' shadow-sm p-4 max-w-64 text-sm font-sans gap-1 border rounded-xl border-gray-300 bg-white text-black flex flex-col justify-center text-left items-center'>
-                <div>
-                  <p className='font-bold text-xl'>Unread Notifications 0</p>
-                  <p className='text-sm'>
-                    Good job! Looks like you're all caught up.{' '}
-                    <Link className='text-white/40' to={path.home}>
-                      View All
-                    </Link>
-                  </p>
-                </div>
+              >
+                <path strokeLinecap='round' strokeLinejoin='round' d='M12 4.5v15m7.5-7.5h-15' />
+              </svg>
+            </div>
+          }
+          show={true}
+          onEvent='onClick'
+          renderPopover={
+            <div className=' shadow-sm max-w-52 w-40  text-sm font-sans gap-1 border rounded-xl border-gray-300 bg-white text-black flex flex-col justify-center text-left items-center'>
+              {' '}
+              <Link to='' className='hover:bg-blue-950 p-2 w-full rounded-xl hover:text-white'>
+                Add New Movie
+              </Link>
+              <Link to='' className='hover:hover:bg-blue-950 hover:text-white p-2 rounded-xl w-full'>
+                Add New TV Show
+              </Link>
+            </div>
+          }
+        />
+
+        <Popover
+          children='VI'
+          className='border border-white w-7 text-center text-white font-bold mx-6 hover:text-black hover:bg-white cursor-pointer'
+          renderPopover={
+            <div className='w-64 h-26 p-3 bg-white border border-gray-200 shadow-sm rounded-sm'>
+              <h2 className='font-bold'>Language Preferences</h2>
+              <div>
+                <select className='mt-2 bg-gray-200/90 rounded-sm shadow-sm w-full p-2'>
+                  <option value='Tiếng việt'>Tiếng việt</option>
+                  <option value='English'>English</option>
+                </select>
               </div>
-            }
-          />
+            </div>
+          }
+          onEvent='onClick'
+          show={true}
+        />
+        <Popover
+          onEvent='onClick'
+          show={true}
+          children={
+            <img
+              className='w-[65px] -translate-x-2'
+              src='https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-441-bell-9cd2af257b98c4af3460078777d8e38a5e12bca89704eeac2f39273afcbd06ff.svg'
+              alt=''
+            />
+          }
+          renderPopover={
+            <div className=' shadow-sm p-4 max-w-64 text-sm font-sans gap-1 border rounded-xl border-gray-300 bg-white text-black flex flex-col justify-center text-left items-center'>
+              <div>
+                <p className='font-bold text-xl'>Unread Notifications 0</p>
+                <p className='text-sm'>
+                  Good job! Looks like you're all caught up.{' '}
+                  <Link className='text-white/40' to={path.home}>
+                    View All
+                  </Link>
+                </p>
+              </div>
+            </div>
+          }
+        />
+        <div>
           <Popover
             renderPopover={
-              <div className='shadow-sm max-w-48 text-sm font-sans gap-1 border rounded-xl border-gray-300 bg-white text-black flex flex-col justify-center text-left items-start'>
+              <div className=' shadow-sm max-w-48 text-sm font-sans gap-1 border rounded-xl border-gray-300 bg-white text-black flex flex-col justify-center text-left items-start'>
                 <div className='p-4 border-b-[1px] border-b-gray-200'>
                   <p className=' text-sm'>minhDevFE120304</p>
                   <p className='text-gray-400'>Xem hồ sơ</p>
@@ -217,70 +217,95 @@ export default function Header() {
                 </div>
               </div>
             }
-            onEvent='onClick'
+            onEvent='onMouseEnter'
             show={true}
-            children={<img src={icons} alt='' className='ml-6 w-7 h-7 rounded-full' />}
+            children={<img src={icons} alt='' className='ml-8 -translate-x-6 w-7 h-7 rounded-full' />}
           />
         </div>
-      </div>
-      <Popover
-        className='w-full mr-2'
-        fullWidth={true}
-        children={
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            fill='none'
-            viewBox='0 0 24 24'
-            strokeWidth={1.5}
-            stroke='#ffff'
-            className='size-6 ml-6'
-          >
-            <path
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              d='m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z'
-            />
-          </svg>
-        }
-        show={true}
-        onEvent='onClick'
-        leaveEvent='onMouseLeave'
-        renderPopover={
-          <div className='w-full bg-gray-500 p-4'>
-            <div className='flex items-center w-full'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                fill='none'
-                viewBox='0 0 24 24'
-                strokeWidth={1.5}
-                stroke='#ffff'
-                className='size-6 mr-2'
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  d='m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z'
-                />
-              </svg>
-              <input
-                type='text'
-                placeholder='Search for a movie, tv show, person....'
-                className='flex-grow p-2 rounded-l-md focus:outline-none'
+        <Popover
+          className='ml-6 -translate-x-5 translate-y-1'
+          fullWidth={true}
+          children={
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              strokeWidth={1.5}
+              stroke='#ffff'
+              className='size-6 ml-6'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z'
               />
-              <button className='bg-gradient-to-r from-green-400 to-blue-500 text-white px-4 py-2 rounded-r-md'>
-                Search
-              </button>
-            </div>
-            <div className='mt-4 space-y-2'>
-              {dataRatedMovies?.slice(0, 10).map((movies: Movie) => (
-                <div key={movies.id} className='text-white'>
-                  {movies.title}
+            </svg>
+          }
+          onEvent='onMouseEnter'
+          leaveEvent='onMouseLeave'
+          renderPopover={
+            <div className='w-full bg-white p-4'>
+              <div className='flex items-center w-full'>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                  strokeWidth={1.5}
+                  stroke='#000'
+                  className='size-6 mr-2'
+                >
+                  <path
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    d='m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z'
+                  />
+                </svg>
+                <input
+                  type='text'
+                  placeholder='Search for a movie, tv show, person....'
+                  className='flex-grow p-2 border border-black rounded-sm shadow-sm rounded-l-md focus:outline-none'
+                />
+                <button className='bg-gradient-to-r from-green-400 to-blue-500 text-white px-4 py-2 rounded-r-md'>
+                  Search
+                </button>
+              </div>
+              <div className='mt-4 space-y-2'>
+                <div className='flex items-center w-full border-b-[1px] border-b-gray-200'>
+                  <img
+                    src='https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-962-trending-d9ba6bdc2bd0f18a8762ed296e3cd58987e1970b08676b690609114d2928dd78.svg'
+                    alt=''
+                    className='size-6 items-center'
+                  />
+                  <div className='text-black font-bold text-xl ml-2'>Trending</div>
                 </div>
-              ))}
+                <div className='flex flex-col gap-3'>
+                  {dataRatedMovies?.slice(0, 10).map((movies: Movie) => (
+                    <div className='flex border-b-gray-200 border-b-[1px]'>
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        fill='none'
+                        viewBox='0 0 24 24'
+                        strokeWidth={1.5}
+                        stroke='#000'
+                        className='size-6 mr-2'
+                      >
+                        <path
+                          strokeLinecap='round'
+                          strokeLinejoin='round'
+                          d='m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z'
+                        />
+                      </svg>
+                      <Link to={path.home} key={movies.id} className='text-black'>
+                        {movies.title}
+                      </Link>
+                    </div>
+                  ))}
+                </div>
+              </div>
             </div>
-          </div>
-        }
-      />
+          }
+        />
+      </div>
     </div>
   )
 }
---
+++ src/Contexts/app.context.ts
@@ -0,0 +1,54 @@
+// import { createContext, useState } from 'react'
+// import { getAccessTokenFromLS, getProfileFormLS } from '../utils/auth'
+// import { User } from '../types/user.type'
+// import { ExtendedPurchases } from '../pages/Cart/Cart'
+
+// interface AppContextInterface {
+//   isAuthenticated: boolean
+//   setIsAuthenticated: React.Dispatch<React.SetStateAction<boolean>>
+//   profile: User | null
+//   setProfile: React.Dispatch<React.SetStateAction<User | null>>
+//   extendedPurchases: ExtendedPurchases[]
+//   setExtendedPurchases: React.Dispatch<React.SetStateAction<ExtendedPurchases[]>>
+//   reset: () => void
+// }
+interface AppContextInterface {
+  profile: User | null
+}
+// const initialAppContext: AppContextInterface = {
+//   isAuthenticated: Boolean(getAccessTokenFromLS()),
+//   setIsAuthenticated: () => null,
+//   profile: getProfileFormLS(),
+//   setProfile: () => null,
+//   extendedPurchases: [],
+//   setExtendedPurchases: () => null,
+//   reset: () => null
+// }
+// export const AppContext = createContext<AppContextInterface>(initialAppContext)
+
+// export const AppProvider = ({ children }: { children: React.ReactNode }) => {
+//   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(initialAppContext.isAuthenticated)
+//   const [extendedPurchases, setExtendedPurchases] = useState<ExtendedPurchases[]>(initialAppContext.extendedPurchases)
+//   const reset = () => {
+//     setIsAuthenticated(false)
+//     setExtendedPurchases([])
+//     setProfile(null)
+//   }
+
+//   const [profile, setProfile] = useState<User | null>(initialAppContext.profile)
+//   return (
+//     <AppContext.Provider
+//       value={{
+//         reset,
+//         isAuthenticated,
+//         setIsAuthenticated,
+//         profile,
+//         setProfile,
+//         extendedPurchases,
+//         setExtendedPurchases
+//       }}
+//     >
+//       {children}
+//     </AppContext.Provider>
+//   )
+// }
